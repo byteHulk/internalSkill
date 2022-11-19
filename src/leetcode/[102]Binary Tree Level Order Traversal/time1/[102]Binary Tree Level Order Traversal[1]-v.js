9186892c4beadd8e910b420212e14610e5f00e13
Hulk <33158585+byteHulk@users.noreply.github.com>
2022-11-19 10:40:14 +0800

    doc:complete leetcode 102

---
+++ src/leetcode/[102]Binary Tree Level Order Traversal/time1/[102]Binary Tree Level Order Traversal[1]-v.js
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode.cn id=102 lang=javascript
+ *
+ * [102] Binary Tree Level Order Traversal
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var levelOrder = function(root) {
+    if (root == null)
+        return []
+    let res = [], queue = []
+    queue.push(root)
+    while (queue.length) {
+        let levl = []
+        let len = queue.length
+        for (let i = 0; i < len; i++) {
+            let tmp = queue.shift()
+            if (tmp.left)
+                queue.push(tmp.left)
+            if (tmp.right)
+                queue.push(tmp.right)
+            levl.push(tmp.val)
+        }
+        res.push(levl)
+    }
+    return res
+};
+// @lc code=end
