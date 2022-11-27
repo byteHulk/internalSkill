fa0cc687a61763cb97f55115b728f763aae3ab0d
Hulk <33158585+byteHulk@users.noreply.github.com>
2022-11-27 10:44:38 +0800

    doc:complete leetcode 700

---
+++ src/leetcode/[700]Search in a Binary Search Tree/time2/[700]Search in a Binary Search Tree[1]-v.js
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode.cn id=700 lang=javascript
+ *
+ * [700] Search in a Binary Search Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} val
+ * @return {TreeNode}
+ */
+var searchBST = function(root, val) {
+    if (!root || root.val === val) return root
+
+    let resL = searchBST(root.left, val)
+    let resR = searchBST(root.right, val)
+  
+    return resL ? resL : resR
+};
+// @lc code=end
