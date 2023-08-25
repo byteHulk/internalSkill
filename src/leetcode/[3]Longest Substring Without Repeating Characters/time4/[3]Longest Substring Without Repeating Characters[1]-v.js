ad6da77c92794d638dfe89840d1db46e18cb174d
Hulk <33158585+byteHulk@users.noreply.github.com>
2023-08-25 10:29:31 +0800

    doc:complete leetcode 3

---
+++ src/leetcode/[3]Longest Substring Without Repeating Characters/time4/[3]Longest Substring Without Repeating Characters[1]-v.js
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode.cn id=3 lang=javascript
+ *
+ * [3] Longest Substring Without Repeating Characters
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function (s) {
+  if (s.length < 2) return s.length
+
+  let visited = {},
+    max = (start = 0)
+
+
+    for(let i in s){
+        const cur = s[i]
+
+        if(cur in visited){
+            // ...
+            start = Math.max(visited[cur] + 1,max)
+        }
+
+        visited[cur] = Number(i)
+
+        max = Math.max(i - start + 1, max)
+    }
+
+    return max
+}
+// @lc code=end
