7789ccb9a2857749495cb914677268dc1deed830
Hulk <33158585+byteHulk@users.noreply.github.com>
2023-12-14 16:11:19 +0800

    doc:complete leetcode 23

---
+++ src/leetcode/[23]Merge k Sorted Lists/time4/[23]Merge k Sorted Lists[1]-v.js
@@ -0,0 +1,52 @@
+/*
+ * @lc app=leetcode.cn id=23 lang=javascript
+ *
+ * [23] Merge k Sorted Lists
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode[]} lists
+ * @return {ListNode}
+ */
+var mergeKLists = function (lists) {
+  const K = lists.length
+
+  if(!K) return null
+
+  let result = lists[0]
+
+  const mergeTwoLists = (l1, l2) => {
+    let dummyNode = new ListNode(0)
+    let tail = dummyNode
+
+    while(l1 && l2){
+        if(l1.val < l2.val){
+            tail.next = l1
+            l1 = l1.next
+        }else{
+            tail.next = l2
+            l2 = l2.next
+        }
+        tail = tail.next
+    }
+
+    tail.next = l1 ? l1 : l2
+
+    return dummyNode.next
+  }
+
+  for(let i = 1;i < K;i++){
+    result = mergeTwoLists(result,lists[i])
+  }
+
+  return result
+}
+// @lc code=end
