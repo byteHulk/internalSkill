8f6f061300161a2e8f3bf977846d64cb9b28ce30
Hulk <33158585+byteHulk@users.noreply.github.com>
2024-01-21 10:41:09 +0800

    doc:complete leetcode 322

---
+++ src/leetcode/[322]Coin Change/time2/[322]Coin Change[1]-v.js
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=322 lang=javascript
+ *
+ * [322] Coin Change
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} coins
+ * @param {number} amount
+ * @return {number}
+ */
+var coinChange = function (coins, amount) {
+  const dp = Array(amount + 1).fill(Infinity)
+  dp[0] = 0
+
+  for (let i = 1; i <= amount; i++) {
+    for (let coin of coins) {
+      if (i - coin >= 0) {
+        dp[i] = Math.min(dp[i - coin] + 1, dp[i])
+      }
+    }
+  }
+  return dp[amount] === Infinity ? -1 : dp[amount]
+}
+// @lc code=end
