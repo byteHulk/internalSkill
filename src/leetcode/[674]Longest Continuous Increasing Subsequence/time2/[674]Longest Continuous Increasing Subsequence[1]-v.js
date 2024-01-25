510ce1dff11c8a029814c3688d9bf33545288d50
Hulk <33158585+byteHulk@users.noreply.github.com>
2024-01-25 14:21:45 +0800

    doc:complete leetcode 674

---
+++ src/leetcode/[674]Longest Continuous Increasing Subsequence/time2/[674]Longest Continuous Increasing Subsequence[1]-v.js
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode.cn id=674 lang=javascript
+ *
+ * [674] Longest Continuous Increasing Subsequence
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findLengthOfLCIS = function (nums) {
+  let start = (max = 0)
+
+  for (let i in nums) {
+    if (nums[i] <= nums[i - 1]) start = i
+
+    max = Math.max(i - start + 1, max)
+  }
+
+  return max
+}
+// @lc code=end
