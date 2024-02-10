0dad6a4bf3c622d2fb93d20da418ad9c70d019ae
Hulk <33158585+byteHulk@users.noreply.github.com>
2024-02-10 11:39:32 +0800

    doc:complete leetcode 72

---
+++ src/leetcode/[72]Edit Distance/time3/[72]Edit Distance[1]-v.js
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode.cn id=72 lang=javascript
+ *
+ * [72] Edit Distance
+ */
+
+// @lc code=start
+/**
+ * @param {string} word1
+ * @param {string} word2
+ * @return {number}
+ */
+var minDistance = function (word1, word2) {
+  let [len1, len2] = [word1.length, word2.length]
+  if (!len1 || !len2) return len1 + len2
+
+  //dp 方程为 word1 的前 i 位到 word2 的前 j 位的编辑距离
+  const dp = Array.from({ length: len1 + 1 }, () =>
+    Array.from({ length: len2 + 1 }, () => 0)
+  )
+
+  for (let i = 0; i <= len1; i++) dp[i][0] = i
+  for (let j = 0; j <= len2; j++) dp[0][j] = j
+
+  for (let i = 1; i <= len1; i++) {
+    for (let j = 1; j <= len2; j++) {
+      if (word1[i - 1] === word2[j - 1]) dp[i][j] = dp[i - 1][j - 1]
+      else dp[i][j] = 1 + Math.min(dp[i - 1][j - 1], dp[i - 1][j], dp[i][j - 1])
+    }
+  }
+
+  return dp[len1][len2]
+}
+// @lc code=end
